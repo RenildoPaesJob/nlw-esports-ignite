ce304e2f56c7fc1e153bcab0426f964352152dc1
Renildo <renildopaesJoob@gmail.com>
2022-09-15 07:14:57 -0300

    finish template web

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,11 +5,14 @@ module.exports = {
     './index.html'
   ],
   theme: {
+    fontFamily:{
+      sans: ['inter', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
-        'nlw-gradient': 'linear-gradient(95.90deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 90.57%)'
-
+        'nlw-gradient': 'linear-gradient(95.90deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 90.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%);'
       }
     },
   },
