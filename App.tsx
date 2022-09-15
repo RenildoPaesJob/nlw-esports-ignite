eaa2d26806f649cb4589466836b6e274f24f0c9f
Renildo <renildopaesJoob@gmail.com>
2022-09-15 08:22:18 -0300

    start mobile

--- App.tsx
+++ App.tsx
@@ -1,50 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-
-interface ButtonProps{
-  title: string,
-}
-
-function Button(props: ButtonProps){
-  return (
-    <>
-      <TouchableOpacity>
-        <Text>
-          {props.title}
-        </Text>
-      </TouchableOpacity>
-    </>
-  )
-}
+import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <Text
-        style={styles.title}
-      >
-        Hello React Native!
-      </Text> */}
-
-      <Button title="send 1"/>
-      <Button title="send 2"/>
-      <Button title="Hello world!"/>
-      <StatusBar style="auto" />
+    <View>
+      
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-
-  container: {
-    flex           : 1,
-    backgroundColor: '#fff',
-    alignItems     : 'center',
-    justifyContent : 'center',
-  },
-  title: {
-    color: '#000',
-    fontSize: 22
-  }
-
-});
---
+++ src/components/Background/index.tsx
@@ -0,0 +1,19 @@
+import { ImageBackground } from 'react-native';
+import { styles } from './styles';
+import BackgroundImg from '../../assets/background-galaxy.png';
+
+interface BackgroundProps {
+  children: React.ReactNode;
+}
+
+export function Background({children}: BackgroundProps) {
+  return (
+    <ImageBackground 
+      source={BackgroundImg}
+      defaultSource={BackgroundImg}
+      style={styles.container}
+    >
+      {children}
+    </ImageBackground>
+  );
+}
